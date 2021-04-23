580ec2852410114e2b23286688f301d7ae3586c3
Sam Prabhu <sameerhassen2410@berkeley.edu>
2021-04-22 21:20:27 -0700

    we done it

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -7,14 +7,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.getElementById("localservercontainer").style.visibility = "hidden";
 
+    // Set up localServerButton
   var localServerButton = document.getElementById('callLocalBtn');
 
   localServerButton.addEventListener('click', async function () {
+    document.getElementById("localservercontainer").style.visibility = "hidden";
+    document.getElementById("localserverdata").innerHTML = "";
     var text = await getData(API_URL);
-    text = text.replaceAll("'", "").replaceAll("[", "").replaceAll("]", "").toUpperCase();
-    document.getElementById("localserverdata").innerHTML = text;
+    var listItems = parseListItems(text);
+    document.getElementById("localserverdata").innerHTML = "<ul class=\"list-group\">" + listItems + "</ul>";
     document.getElementById("localservercontainer").style.visibility = "visible";
   })
+
+  // Function to capitalize a string
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+// Return Bootstrap 4 list item color based on label
+const getListItemType = (item) => {
+  const ORANGE_CLASSES = ['satire', 'unreliable', 'junksci',
+    'rumor', 'clickbait', 'political', 'bias'];
+  const RED_CLASSES = ['fake', 'hate', 'conspiracy'];
+  const GREEN_CLASSES = ['reliable',];
+  const BLUE_CLASSES = ['unknown'];
+
+  if (ORANGE_CLASSES.includes(item)) {
+    return "list-group-item-warning";
+  } else if (RED_CLASSES.includes(item)) {
+    return "list-group-item-danger";
+  } else if (GREEN_CLASSES.includes(item)) {
+    return "list-group-item-success";
+  } else if (BLUE_CLASSES.includes(item)) {
+    return "list-group-item-primary";
+  }
+
+  return "";
+}
+
+const parseListItems = (text) => {
+  var items = "";
+  for (let val in text.split(";")) {
+    valText = text.split(";")[val];
+    valText = valText.replace(/\s+/g, ' ').trim();
+    if (valText !== "") {
+      items += "<li class=\"list-group-item " + getListItemType(valText) + "\">";
+      items += (Number(val) + 1) + ": " + capitalize(valText) + "</li>";
+    }
+  }
+  return items;
+}
 
   async function getData(url) {
     articleText = document.getElementById("textBox").value;
